2acae47323e5f298a346e37b7a3220567e4cf1f6
mmlmml1 <huqingyang2000@gmail.com>
2017-09-05 22:14:44 +0800

    Update UI on certification view

--- general/UserCertification.swift
+++ general/UserCertification.swift
@@ -10,16 +10,17 @@ import Foundation
 import UIKit
 import Alamofire
 
-class UserCertificationView:UIViewController{
-    @IBOutlet weak var status: UILabel!
+class UserCertificationView:UIViewController,UITableViewDelegate,UITableViewDataSource{
+    @IBOutlet weak var tableview: UITableView!
+    
     @IBOutlet weak var enterButton: UIButton!
+    var statusData = [String:String]()
+    let ID = "cell"
     override func viewDidLoad() {
-        super.viewDidLoad()
-        self.status.numberOfLines = 0;
+        tableview.register(DetailCell.self, forCellReuseIdentifier: ID)
         enterButton.isEnabled = false
         checkVersion()
     }
-    
     func checkVersion(){
         let parameters:Parameters = [
             "version":Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
@@ -33,7 +34,8 @@ class UserCertificationView:UIViewController{
                 let code = (json as! [String:Int])["code"]
                 //print(code)
                 if(code == 200){
-                    self.getGeneralInformation()
+                    self.enterButton.isEnabled = true
+                    self.loadData()
                 } else {
                     let alert = UIAlertController(title: "错误", message: "联网检测本地认证数据库版本失败！请尝试将App升级至最新版本后再试。", preferredStyle: .alert)
                     let ok = UIAlertAction(title: "好的", style: .default, handler: nil)
@@ -46,34 +48,45 @@ class UserCertificationView:UIViewController{
             
         })
     }
-    
-    func getGeneralInformation() {
+    func loadData(){
+        statusData.removeAll()
         let headers: HTTPHeaders = [
             "Cookie" : "token=" + UserDefaults.standard.string(forKey: "token")!
         ]
-        Alamofire.request("https://api.nfls.io/alumni/auth/status", headers: headers).responseJSON(completionHandler: {
-            response in
+        Alamofire.request("https://api.nfls.io/alumni/auth/status", headers: headers).responseJSON { response in
             switch(response.result){
             case .success(let json):
-                if((json as! [String:AnyObject])["code"] as! Int == 200){
-                    self.enterButton.isEnabled = true
-                    let messages = (json as! [String:AnyObject])["message"] as! [String]
-                    for message in messages{
-                        DispatchQueue.main.async {
-                            self.status.text = self.status.text! + message as String! + "\n"
-                        }
+                if((json as! [String:AnyObject])["code"] as! Int) == 200 {
+                    let info = ((json as! [String:AnyObject])["message"]! as! [[String:String]])
+                    for status in info{
+                        self.statusData[status["title"]!] = status["content"]!
                     }
+                    self.tableview.delegate = self
+                    self.tableview.dataSource = self
+                    self.tableview.reloadData()
                 }
+                break
             default:
                 break
-                
             }
-        })
-        
+        }
     }
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ID, for: indexPath as IndexPath)
+        let key = Array(statusData.keys)[indexPath.row]
+        let value = Array(statusData.values)[indexPath.row]
+        cell.textLabel!.text = key
+        cell.detailTextLabel!.text = value
+        cell.detailTextLabel!.lineBreakMode = .byWordWrapping
+        cell.detailTextLabel!.numberOfLines = 0
+        cell.detailTextLabel!.sizeToFit()
+        cell.sizeToFit()
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return statusData.count
+    }
     @IBAction func backToAlumni(segue: UIStoryboardSegue){
         
     }
-
 }
