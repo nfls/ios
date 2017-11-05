7798620652cc779f83e1820c7f718abec2bfcc65
mmlmml1 <huqingyang2000@gmail.com>
2017-11-05 19:19:01 +0800

    Add image on notification

--- pushNotification/NotificationService.swift
+++ pushNotification/NotificationService.swift
@@ -16,18 +16,21 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
+            if let urlString = bestAttemptContent.userInfo["imageUrl"] as? String, let data = NSData(contentsOf: URL(string:urlString)!) as Data? {
+                let path = NSTemporaryDirectory() + "attachment"
+                _ = FileManager.default.createFile(atPath: path, contents: data, attributes: nil)
+                do {
+                    let file = URL(fileURLWithPath: path)
+                    let attachment = try UNNotificationAttachment(identifier: "attachment", url: file,options:[UNNotificationAttachmentOptionsTypeHintKey : "public.jpeg"])
+                    bestAttemptContent.attachments = [attachment]
+                } catch {}
+            }
             contentHandler(bestAttemptContent)
         }
     }
     
     override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
