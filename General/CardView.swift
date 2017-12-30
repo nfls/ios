0602d8fcd11115b1310200ec4d9be203755b3a21
mmlmml1 <huqingyang2000@gmail.com>
2017-12-30 12:11:52 +0800

    Add viewcontroller
    but autocomplete is broken again...

---
+++ General/CardView.swift
@@ -0,0 +1,17 @@
+//
+//  CardView.swift
+//  NFLSers-iOS
+//
+//  Created by hqy on 2017/12/30.
+//  Copyright © 2017年 胡清阳. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import EFQRCode
+
+class CardViewController:UIViewController{
+    override func viewdidload(){
+        
+    }
+}
